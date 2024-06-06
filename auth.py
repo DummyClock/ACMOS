fd84993a9c6533447b51eb54da61c57416072b6e
MinerBryan <49237020+MinerBryan@users.noreply.github.com>
2024-06-05 21:58:57 -0400

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,6 @@
+'''
+Use this file for storing sensitive information, like a webhook or login credentials.
+This way we can keep the sensitive info in our workspace and only push the updated code/files to git,
+where we can then download onto our personal device without worrying about adding our info back
+'''
+WEBHOOK_URL = ""
