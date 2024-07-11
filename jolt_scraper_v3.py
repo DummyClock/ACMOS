bdc277b322168ecfb6183c1889b234a2109d1fec
Elias S <92802691+DummyClock@users.noreply.github.com>
2024-07-11 02:06:28 -0400

    Update jolt_scraper_v3.py

--- jolt_scraper_v3.py
+++ jolt_scraper_v3.py
@@ -9,7 +9,7 @@ import time
 # from auth import EMAIL, PASSWORD
 
 # Gets hidden values from Github Secrets - (Remove this block when testing on a locally)
-EMAIL = os.environ{'EMAIL']
+EMAIL = os.environ{'EMAIL'}
 PASSWORD = os.environ{'PASSWORD'}
 
 def downloadCSVs(listNames, startDate=None, endDate=None):
