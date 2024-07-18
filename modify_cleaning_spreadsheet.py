caddeb5462f7c0d03c08e7eb543537ac33ceb2f5
Elias S. <92802691+DummyClock@users.noreply.github.com>
2024-07-18 01:13:29 -0400

    Update modify_cleaning_spreadsheet.py
    Commented out the auth import statement. Instead, the variables within that import will be read from Github Secrets

--- modify_cleaning_spreadsheet.py
+++ modify_cleaning_spreadsheet.py
@@ -5,13 +5,11 @@ import time
 from google.oauth2.service_account import Credentials
 from datetime import datetime, timedelta
 
-from auth import SERVICE_KEY_JSON_FILE, SPREADSHEET_ID, MASTER_SPREADSHEET_ID
+# from auth import SERVICE_KEY_JSON_FILE, SPREADSHEET_ID, MASTER_SPREADSHEET_ID
 
-'''
 SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
 MASTER_SPREADSHEET_ID = os.environ['MASTER_SPREADSHEET_ID']
 SERVICE_KEY_JSON_FILE = os.environ['SERVICE_KEY_JSON_FILE']
-'''
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 creds = Credentials.from_service_account_info(SERVICE_KEY_JSON_FILE, scopes=SCOPES)
