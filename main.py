4102e2989a4474755e1cf9492ed5af80c20b06e0
Elias S. <92802691+DummyClock@users.noreply.github.com>
2024-07-19 05:16:00 -0400

    Update and rename test ss_manip.py to main.py
    Prototype main function that tests both jolt_scraper_v4 & ss_manip

--- main.py
+++ main.py
@@ -2,25 +2,30 @@ import os
 import gspread
 import time
 from ss_manip import readCSVFiles
+from jolt_scraper_v4 import downloadCSVs
 from google.oauth2.service_account import Credentials
 
 
 # Test functions
 if __name__ == '__main__':
     try:
-        path = os.path.dirname(os.path.realpath(__file__)) + '\\tmp'
-        print(path)
-
         # Set up credentials
-        # from auth import SERVICE_KEY_JSON_FILE, SPREADSHEET_ID, MASTER_SPREADSHEET_ID
+        from auth import SERVICE_KEY_JSON_FILE, SPREADSHEET_ID, MASTER_SPREADSHEET_ID
+        '''
         SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
         MASTER_SPREADSHEET_ID = os.environ['MASTER_SPREADSHEET_ID']
         SERVICE_KEY_JSON_FILE = os.environ['SERVICE_KEY_JSON_FILE']
+        '''
         SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
         creds = Credentials.from_service_account_info(SERVICE_KEY_JSON_FILE, scopes=SCOPES)
         client = gspread.authorize(creds)
 
+        #Testing the functions. (Downloads files & lists names of downloaded files)
+        listName = ["BOH Cleaning List".lower()]
+        #path = os.path.dirname(os.path.realpath(__file__)) + '\\tmp'
+
         #Prints a dictionary of the important data types
+        path = downloadCSVs(listName)
         print(readCSVFiles(path, client, SPREADSHEET_ID, MASTER_SPREADSHEET_ID))
 
     except gspread.exceptions.APIError as e:
@@ -29,11 +34,10 @@ if __name__ == '__main__':
             print("Momentairly unable to access Google Sheets API. Will attempt to access again. \n\tPlease wait a few minutes...")
             time.sleep(240) #4 minutes
             print("Second attempt commencing...\n\n")
-            readCSVFiles(path)
+            readCSVFiles(path, client, SPREADSHEET_ID, MASTER_SPREADSHEET_ID)
         except gspread.exceptions.APIError as e:
             print("Still unable to access API. Please check if quota limit has been reached.")
         except FileNotFoundError as e:
             print("The file path '" + path + "' doesn't exist within this system.\nMake sure " + os.path.dirname(os.path.realpath(__file__)) + " has a 'tmp' folder within.")
     except FileNotFoundError as e:
         print("The file path '" + path + "' doesn't exist within this system.\nMake sure " + os.path.dirname(os.path.realpath(__file__)) + " has a 'tmp' folder within.")
-    
