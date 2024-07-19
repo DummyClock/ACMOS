f3090a9c4a9699cda7357344f3bd59dfe39fb509
Elias S. <92802691+DummyClock@users.noreply.github.com>
2024-07-18 20:26:39 -0400

    Minor modifications to test ss_manip.py

--- test ss_manip.py
+++ test ss_manip.py
@@ -12,12 +12,10 @@ if __name__ == '__main__':
         print(path)
 
         # Set up credentials
-        from auth import SERVICE_KEY_JSON_FILE, SPREADSHEET_ID, MASTER_SPREADSHEET_ID
-        '''
+        # from auth import SERVICE_KEY_JSON_FILE, SPREADSHEET_ID, MASTER_SPREADSHEET_ID
         SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
         MASTER_SPREADSHEET_ID = os.environ['MASTER_SPREADSHEET_ID']
         SERVICE_KEY_JSON_FILE = os.environ['SERVICE_KEY_JSON_FILE']
-        '''
         SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
         creds = Credentials.from_service_account_info(SERVICE_KEY_JSON_FILE, scopes=SCOPES)
         client = gspread.authorize(creds)
