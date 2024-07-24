92574ee21e7be4ccf55588423445c037b7c4e6b7
Elias S. <92802691+DummyClock@users.noreply.github.com>
2024-07-24 01:23:39 -0400

    Updated main.py to be up to date
    - tests downloadCSVs() & readCSVFiles()

--- main.py
+++ main.py
@@ -7,37 +7,20 @@ from google.oauth2.service_account import Credentials
 
 
 # Test functions
-if __name__ == '__main__':
-    try:
-        # Set up credentials
-        from auth import SERVICE_KEY_JSON_FILE, SPREADSHEET_ID, MASTER_SPREADSHEET_ID
-        '''
+from auth import SERVICE_KEY_JSON_FILE, SPREADSHEET_ID, MASTER_SPREADSHEET_ID
+'''
         SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
         MASTER_SPREADSHEET_ID = os.environ['MASTER_SPREADSHEET_ID']
         SERVICE_KEY_JSON_FILE = os.environ['SERVICE_KEY_JSON_FILE']
-        '''
-        SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-        creds = Credentials.from_service_account_info(SERVICE_KEY_JSON_FILE, scopes=SCOPES)
-        client = gspread.authorize(creds)
+'''
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+creds = Credentials.from_service_account_info(SERVICE_KEY_JSON_FILE, scopes=SCOPES)
+client = gspread.authorize(creds)
 
-        #Testing the functions. (Downloads files & lists names of downloaded files)
-        listName = ["BOH Cleaning List".lower()]
-        #path = os.path.dirname(os.path.realpath(__file__)) + '\\tmp'
+#Testing the functions. (Downloads files & lists names of downloaded files)
+listName = ["BOH Cleaning List".lower()]
+path = os.path.dirname(os.path.realpath(__file__)) + '\\tmp'
 
-        #Prints a dictionary of the important data types
-        path = downloadCSVs(listName)
-        print(readCSVFiles(path, client, SPREADSHEET_ID, MASTER_SPREADSHEET_ID))
-
-    except gspread.exceptions.APIError as e:
-        try:
-            time.sleep(4)
-            print("Momentairly unable to access Google Sheets API. Will attempt to access again. \n\tPlease wait a few minutes...")
-            time.sleep(240) #4 minutes
-            print("Second attempt commencing...\n\n")
-            readCSVFiles(path, client, SPREADSHEET_ID, MASTER_SPREADSHEET_ID)
-        except gspread.exceptions.APIError as e:
-            print("Still unable to access API. Please check if quota limit has been reached.")
-        except FileNotFoundError as e:
-            print("The file path '" + path + "' doesn't exist within this system.\nMake sure " + os.path.dirname(os.path.realpath(__file__)) + " has a 'tmp' folder within.")
-    except FileNotFoundError as e:
-        print("The file path '" + path + "' doesn't exist within this system.\nMake sure " + os.path.dirname(os.path.realpath(__file__)) + " has a 'tmp' folder within.")
+#Prints a dictionary of the important data types
+path = downloadCSVs(listName)
+print(readCSVFiles(path, client, SPREADSHEET_ID, MASTER_SPREADSHEET_ID))
