57106d5293c5207e369ff1e49f8e8db6e87014ad
MinerBryan <49237020+MinerBryan@users.noreply.github.com>
2024-07-25 15:20:29 -0400

    Comment changes and slight organization alterations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import gspread
-import time
+import time, os
 from datetime import date
 from ss_manip import readCSVFiles
 from jolt_scraper_v4 import downloadCSVs
@@ -7,8 +7,7 @@ from activeTask_manip import prelim_activeTaskModify, post_activeTaskModify
 from google.oauth2.service_account import Credentials
 
 
-# Test functions
-# Set up credentials
+# Set up credentials and other Variables
 #from auth import SERVICE_KEY_JSON_FILE, SPREADSHEET_ID, MASTER_SPREADSHEET_ID
 SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
 MASTER_SPREADSHEET_ID = os.environ['MASTER_SPREADSHEET_ID']
@@ -18,20 +17,18 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 creds = Credentials.from_service_account_info(SERVICE_KEY_JSON_FILE, scopes=SCOPES)
 client = gspread.authorize(creds)
 
+listNames = ["BOH Cleaning List"]
 
-print("hello")
-time.sleep(5)
-
-#Testing the functions. (Downloads files & lists names of downloaded files)
-listName = ["BOH Cleaning List"]
-#print("Testing prelim secondary function")
+#Update Status Codes of tasks in Active Task Spreadsheet
 prelim_activeTaskModify(client, SPREADSHEET_ID)
 time.sleep(5)
 
-#print("Testing primary functions")
-path = downloadCSVs(listName)  
+#Download Completed Jolt List Data
+path = downloadCSVs(listNames)  
+
+#Gathers a dictionary of cleaned tasks and updates cleaning dates
 result = readCSVFiles(path, client, SPREADSHEET_ID, MASTER_SPREADSHEET_ID)       #Prints a dictionary of the important data types
 time.sleep(5)
 
-#print(result)
+#Update the Active Task Spreadsheet with upcoming tasks and removes completed tasks
 post_activeTaskModify(client, SPREADSHEET_ID, result, date(2024,7,31), date(2024,8,20))
